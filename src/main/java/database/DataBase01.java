39120af72b3af8faa933b7423cda83bcde55d74c
camillabarreto <camillabarretodesousa@gmail.com>
2019-12-05 19:16:20 -0300

    adicionado replicas das classes Service(com param de Path diferente) e DataBase

---
+++ src/main/java/database/DataBase01.java
@@ -0,0 +1,84 @@
+package database;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class DataBase01 {
+    private static DataBase01 instance;
+    private ArrayList<Account> accounts;
+    private ArrayList<Replica> replicas;
+    private ArrayList<Operation> operations; //lista temporaria
+    private ArrayList<Action> actions;       //lista permanente
+    private boolean coordinator;
+    private long seed;
+    private Random gerador;
+
+    public DataBase01() {
+        this.accounts = new ArrayList<>();
+        this.replicas = new ArrayList<>();
+        this.operations = new ArrayList<>();
+        this.actions = new ArrayList<>();
+        this.coordinator = false;
+        this.seed = 0;
+        this.gerador = new Random();
+
+        this.accounts.add(new Account("1234", "100,00"));
+        this.accounts.add(new Account("4345", "50,00"));
+        this.accounts.add(new Account("5678", "250,00"));
+    }
+
+    public static DataBase01 getInstance() {
+        if(instance == null){
+            instance = new DataBase01();
+        }
+        return instance;
+    }
+
+    public ArrayList<Account> getAccounts() {
+        return accounts;
+    }
+
+    public ArrayList<Replica> getReplicas() {
+        return replicas;
+    }
+
+    public ArrayList<Action> getActions(){ return actions; }
+
+    public void setReplicas(ArrayList<Replica> replicas) {
+        this.replicas = replicas;
+    }
+
+    public void addOperation(Operation operation) {
+        this.operations.add(operation);
+    }
+
+    public void removeOperation(String id) {
+        for(Operation operation : operations){
+            if(operation.getId().equals(id)){
+                this.operations.remove(operation);
+                return;
+            }
+        }
+    }
+
+    public boolean isCoordinator(){ return this.coordinator; }
+
+    public void setCoordinator(boolean coordinator){ this.coordinator = coordinator; }
+
+    public float getRandomNumber() {
+        return this.gerador.nextFloat();
+    }
+
+    public void setSeed(int seed) {
+        this.gerador = new Random(seed);
+    }
+
+    public void clearReplicas(){
+        this.replicas.clear();
+    }
+
+    public void addActions(String id, String status) {
+        this.actions.add(new Action(id, status));
+    }
+
+}
---
+++ src/main/java/rest/Service01.java
@@ -0,0 +1,162 @@
+package rest;
+
+import database.*;
+import org.glassfish.jersey.client.ClientConfig;
+
+import javax.ws.rs.*;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
+
+@Path("service01")
+public class Service01 {
+
+    private DataBase01 dataBase = DataBase01.getInstance();
+    String name = "service01";
+
+    /***********************************************************/
+    private ClientConfig config = new ClientConfig();
+    private Client client = ClientBuilder.newClient(config);
+    /***********************************************************/
+
+    @Path("accounts")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAccounts() {
+        HashMap<String, ArrayList<Account>> hashMap = new HashMap<>();
+        hashMap.put("contas", this.dataBase.getAccounts());
+        return Response.ok(hashMap).build();
+    }
+
+    @Path("replicas")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getReplicas() {
+        HashMap<String, ArrayList<Replica>> hashMap = new HashMap<>();
+        hashMap.put("replicas", dataBase.getReplicas());
+        return Response.ok(hashMap).build();
+    }
+
+    @Path("replicas")
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response setReplicas(HashMap<String, ArrayList<Replica>> hashMap) {
+        dataBase.setReplicas(hashMap.get("replicas"));
+        dataBase.setCoordinator(true);
+        return Response.ok().build();
+    }
+
+    @Path("replicas")
+    @DELETE
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response clearReplicas(HashMap<String, ArrayList<Replica>> hashMap) {
+        dataBase.clearReplicas();
+        dataBase.setCoordinator(false);
+        return Response.ok().build();
+    }
+
+    @Path("operation")
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response addOperation(Operation operation){
+        if(dataBase.isCoordinator()){
+            System.out.println("sou coordenador "+name);
+            dataBase.addOperation(operation);
+            boolean flag = true;
+            for(Replica replica : dataBase.getReplicas()){
+                WebTarget t = client.target(UriBuilder.fromUri(replica.getEndpoint()).build());
+                Response response = t.path(replica.getId()).path("operation").request(MediaType.TEXT_PLAIN + ";charset=utf-8").put(Entity.entity(operation,MediaType.APPLICATION_JSON), Response.class);
+                System.out.println("Status de resposta: "+response.getStatus());
+
+                if(response.getStatus() == 403){
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag){
+                //enviar decisao PUT
+                System.out.println("\nDecisão PUT");
+                //realizar a operação
+                this.dataBase.addActions(operation.getId(), "success");
+                return Response.ok().build();
+            }else{
+                //enviar decisao DELETE
+                System.out.println("\nDecisão DELETE");
+                this.dataBase.addActions(operation.getId(), "fail");
+                return Response.status(403).build();
+            }
+        }else{
+            System.out.println("NAO sou coordenador "+name);
+            this.dataBase.addOperation(operation);
+            float number = dataBase.getRandomNumber();
+            System.out.println("number: "+number);
+            if(number <= 0.7){
+                return Response.ok().build();
+            }else{
+                return Response.status(403).build();
+            }
+        }
+    }
+
+    @Path("decision")
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response commitDecision(String id){
+        if(dataBase.isCoordinator()){
+            return Response.status(400).build();
+        }
+        this.dataBase.removeOperation(id);
+        this.dataBase.addActions(id, "success");
+        return Response.ok().build();
+    }
+
+    @Path("decision")
+    @DELETE
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response rollbackDecision(String id){
+        if(dataBase.isCoordinator()){
+            return Response.status(400).build();
+        }
+        this.dataBase.removeOperation(id);
+        this.dataBase.addActions(id, "fail");
+        return Response.ok().build();
+    }
+
+    @Path("historic")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getHistoric() {
+        HashMap<String, ArrayList<Action>> hashMap = new HashMap<>();
+        hashMap.put("acoes", this.dataBase.getActions());
+        return Response.ok(hashMap).build();
+    }
+
+    @Path("seed")
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response loadSeed(int seed) {
+        dataBase.setSeed(seed);
+        if (dataBase.isCoordinator()){
+            Replica replica = dataBase.getReplicas().get(0);
+            WebTarget t = client.target(UriBuilder.fromUri(replica.getEndpoint()).build());
+            Response response = t.path(replica.getId()).path("seed").request().post(Entity.entity(12322,MediaType.APPLICATION_JSON), Response.class);
+            System.out.println("Status de resposta: "+response.getStatus());
+
+            replica = dataBase.getReplicas().get(1);
+            t = client.target(UriBuilder.fromUri(replica.getEndpoint()).build());
+            response = t.path(replica.getId()).path("seed").request().post(Entity.entity(12333,MediaType.APPLICATION_JSON), Response.class);
+            System.out.println("Status de resposta: "+response.getStatus());
+
+            return Response.ok().build();
+        }
+        return Response.ok().build();
+    }
+
+}
